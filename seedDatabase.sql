86a59e494f86880bb885bf3263fa435f83fea039
jjkaddoura <jjkaddoura@umail.ucsb.edu>
2016-11-30 23:13:30 -0800

    Wrote Insert statements to insert provided sample data to DBMS

--- seedDatabase.sql
+++ seedDatabase.sql
@@ -1,7 +1,7 @@
 INSERT ALL
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Tim Cook', 'Timcook@gmail.com', 'TCook1', '4512756891', 'CookApple', 1)
-	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Ariana Grande','AGrande@yahoo.com','AGrande1','9376498172','Ariana Grande',1)
-	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Elon Musk','Elonmusk@yahoo.com','EMusk1','2847201343','Elon Musk',0)
+	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Ariana Grande','AGrande@yahoo.com','AGrande1','9376498172','AGrande@yahoo.com',1)
+	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Elon Musk','Elonmusk@yahoo.com','EMusk1','2847201343','Elonmusk@yahoo.com',0)
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Chris Brown','ChrisBrown@gmail.com','ChrisBro1','4008801345','CB',0)
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Russell Westbrook','RWestbrook@gmail.com','RWesbrook1','2318760989','Thunder Leader',0)
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Kevin Durant','DurantKev@gmail.com','KDurant1','5345298112','KDTrey5',0)
@@ -9,7 +9,7 @@ INSERT ALL
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Mariah Carey','MariaCarey@gmail.com','Mcarey1','5540390182','MariahCarey',0)
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Nicki Minaj','NickiMinaj@gmail.com','Nminaj1','4764033082','NickiMalaji',0)
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Daniel Radcliffe','DRadcliffe@gmail.com','DRad1','2734063052','HP_Daniel',0)
-	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Rihanna','Rihanna@gmail.com','Rihanna1','2723060542','Rihanna',0)
+	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Rihanna','Rihanna@gmail.com','Rihanna1','2723060542','Rihanna@gmail.com',0)
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Justin Bieber','JBieber@gmail.com','JBieber1','8340740172','JBtheKing',0)
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Larry Page','LarryPage@gmail.com','LarryPage1','6540320172','LarryPage',0)
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Stephen Curry','SCurry@gmail.com','SCurry1','3412377584','WarriorChampion',0)
@@ -21,3 +21,226 @@ INSERT ALL
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('James Harden','JHarden@gmail.com','JHarden1','3416274384','James Harden',0)
 	INTO UserProfile (name, email, password, phone_num, screenname, is_manager) VALUES ('Jeff Bezos','JeffBezos@yahoo.com','JBezos1','3463712649','AmazonPrime',0)
 SELECT 1 FROM DUAL;
+
+INSERT ALL 
+	INTO UserProfile_Topic (email, keyword) VALUES ('ChrisBrown@gmail.com','singer')
+	INTO UserProfile_topic (email, keyword) VALUES ('ChrisBrown@gmail.com','rapper')
+	INTO UserProfile_topic (email, keyword) VALUES ('ChrisBrown@gmail.com','dancer')
+	INTO UserProfile_topic (email, keyword) VALUES ('RWestbrook@gmail.com','Basketball')
+	INTO UserProfile_topic (email, keyword) VALUES ('RWestbrook@gmail.com','thunder')
+	INTO UserProfile_topic (email, keyword) VALUES ('RWestbrook@gmail.com','NBA')
+	INTO UserProfile_topic (email, keyword) VALUES ('DurantKev@gmail.com','Basketball')
+	INTO UserProfile_topic (email, keyword) VALUES ('DurantKev@gmail.com','warrior')
+	INTO UserProfile_topic (email, keyword) VALUES ('DurantKev@gmail.com','NBA')
+	INTO UserProfile_topic (email, keyword) VALUES ('Iggy@gmail.com','singer')
+	INTO UserProfile_topic (email, keyword) VALUES ('Iggy@gmail.com','rapper')
+	INTO UserProfile_topic (email, keyword) VALUES ('Iggy@gmail.com','model')
+	INTO UserProfile_topic (email, keyword) VALUES ('MariaCarey@gmail.com','signer')
+	INTO UserProfile_topic (email, keyword) VALUES ('NickiMinaj@gmail.com','singer')
+	INTO UserProfile_topic (email, keyword) VALUES ('NickiMinaj@gmail.com','rapper')
+	INTO UserProfile_topic (email, keyword) VALUES ('DRadcliffe@gmail.com','actor')
+	INTO UserProfile_topic (email, keyword) VALUES ('DRadcliffe@gmail.com','Harry Potter')
+	INTO UserProfile_topic (email, keyword) VALUES ('Rihanna@gmail.com','singer')
+	INTO UserProfile_topic (email, keyword) VALUES ('Rihanna@gmail.com','barbardian')
+	INTO UserProfile_topic (email, keyword) VALUES ('JBieber@gmail.com','singer')
+	INTO UserProfile_topic (email, keyword) VALUES ('JBieber@gmail.com','Canadian')
+	INTO UserProfile_topic (email, keyword) VALUES ('LarryPage@gmail.com','google')
+	INTO UserProfile_topic (email, keyword) VALUES ('LarryPage@gmail.com','ceo')
+	INTO UserProfile_topic (email, keyword) VALUES ('LarryPage@gmail.com','computer scientist')
+	INTO UserProfile_topic (email, keyword) VALUES ('SCurry@gmail.com','Basketball')
+	INTO UserProfile_topic (email, keyword) VALUES ('SCurry@gmail.com','warrior')
+	INTO UserProfile_topic (email, keyword) VALUES ('SCurry@gmail.com','NBA')
+	INTO UserProfile_topic (email, keyword) VALUES ('EmmaWatson@yahoo.com','actress')
+	INTO UserProfile_topic (email, keyword) VALUES ('Lheadey@yahoo.com','actress')
+	INTO UserProfile_topic (email, keyword) VALUES ('Lheadey@yahoo.com','Game of Thrones')
+	INTO UserProfile_topic (email, keyword) VALUES ('AGrande@yahoo.com','singer')
+	INTO UserProfile_topic (email, keyword) VALUES ('AGrande@yahoo.com','actress')
+	INTO UserProfile_topic (email, keyword) VALUES ('KHarin@gmail.com','actor')
+	INTO UserProfile_topic (email, keyword) VALUES ('KHarin@gmail.com','Game of Thrones')
+	INTO UserProfile_topic (email, keyword) VALUES ('KHarin@gmail.com','You_Know_Nothing')
+	INTO UserProfile_topic (email, keyword) VALUES ('EClarke@gmail.com','actress')
+	INTO UserProfile_topic (email, keyword) VALUES ('EClarke@gmail.com','game of thrones')
+	INTO UserProfile_topic (email, keyword) VALUES ('PDinklage@gmail.com','actor')
+	INTO UserProfile_topic (email, keyword) VALUES ('PDinklage@gmail.com','game of thrones')
+	INTO UserProfile_topic (email, keyword) VALUES ('JHarden@gmail.com','Basketball')
+	INTO UserProfile_topic (email, keyword) VALUES ('JHarden@gmail.com','rockets')
+	INTO UserProfile_topic (email, keyword) VALUES ('JHarden@gmail.com','NBA')
+	INTO UserProfile_topic (email, keyword) VALUES ('Timcook@gmail.com','apple')
+	INTO UserProfile_topic (email, keyword) VALUES ('Timcook@gmail.com','ceo')
+	INTO UserProfile_topic (email, keyword) VALUES ('Elonmusk@yahoo.com','space x')
+	INTO UserProfile_topic (email, keyword) VALUES ('Elonmusk@yahoo.com','tesla')
+	INTO UserProfile_topic (email, keyword) VALUES ('Elonmusk@yahoo.com','SolarCity')
+	INTO UserProfile_topic (email, keyword) VALUES ('Elonmusk@yahoo.com','OpenAI')
+	INTO UserProfile_topic (email, keyword) VALUES ('JeffBezos@yahoo.com','Amazon')
+	INTO UserProfile_topic (email, keyword) VALUES ('JeffBezos@yahoo.com','ceo')
+SELECT 1 FROM DUAL;
+
+INSERT ALL
+	INTO TopicWord (keyword) VALUES ('actor')
+	INTO TopicWord (keyword) VALUES ('actress')
+	INTO TopicWord (keyword) VALUES ('apple')
+	INTO TopicWord (keyword) VALUES ('amazon')
+	INTO TopicWord (keyword) VALUES ('barbadian')
+	INTO TopicWord (keyword) VALUES ('Basketball')
+	INTO TopicWord (keyword) VALUES ('canadian')
+	INTO TopicWord (keyword) VALUES ('ceo')
+	INTO TopicWord (keyword) VALUES ('computer scientist')
+	INTO TopicWord (keyword) VALUES ('dancer')
+	INTO TopicWord (keyword) VALUES ('game of thrones')
+	INTO TopicWord (keyword) VALUES ('google')
+	INTO TopicWord (keyword) VALUES ('Harry Potter')
+	INTO TopicWord (keyword) VALUES ('model')
+	INTO TopicWord (keyword) VALUES ('nba')
+	INTO TopicWord (keyword) VALUES ('OpenAI')
+	INTO TopicWord (keyword) VALUES ('rapper')
+	INTO TopicWord (keyword) VALUES ('rockets')
+	INTO TopicWord (keyword) VALUES ('singer')
+	INTO TopicWord (keyword) VALUES ('space x')
+	INTO TopicWord (keyword) VALUES ('SolarCity')
+	INTO TopicWord (keyword) VALUES ('tesla')
+	INTO TopicWord (keyword) VALUES ('warrior')
+	INTO TopicWord (keyword) VALUES ('You_Know_Nothing')
+	INTO TopicWord (keyword) VALUES ('veterans')
+	INTO TopicWord (keyword) VALUES ('apple')
+	INTO TopicWord (keyword) VALUES ('freedom')
+	INTO TopicWord (keyword) VALUES ('poster')
+	INTO TopicWord (keyword) VALUES ('Disney')
+	INTO TopicWord (keyword) VALUES ('movie')
+	INTO TopicWord (keyword) VALUES ('street')
+	INTO TopicWord (keyword) VALUES ('city')	
+	INTO TopicWord (keyword) VALUES ('Christmas')	
+	INTO TopicWord (keyword) VALUES ('TV Series')	
+
+SELECT 1 FROM DUAL;
+	
+INSERT ALL
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('RWestbrook@gmail.com','DurantKev@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('RWestbrook@gmail.com','JHarden@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('RWestbrook@gmail.com','SCurry@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('DurantKev@gmail.com','JHarden@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('DurantKev@gmail.com','SCurry@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('JHarden@gmail.com','SCurry@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('KHarin@gmail.com','EClarke@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('KHarin@gmail.com','PDinklage@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('KHarin@gmail.com','Lheadey@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('EClarke@gmail.com','PDinklage@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('EClarke@gmail.com','Lheadey@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('PDinklage@gmail.com','Lheadey@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('ChrisBrown@gmail.com','JBieber@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('ChrisBrown@gmail.com','Iggy@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('ChrisBrown@gmail.com','Rihanna@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('ChrisBrown@gmail.com','AGrande@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('ChrisBrown@gmail.com','NickiMinaj@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('ChrisBrown@gmail.com','MariaCarey@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('JBieber@gmail.com','Iggy@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('JBieber@gmail.com','Rihanna@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('JBieber@gmail.com','AGrande@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('JBieber@gmail.com','NickiMinaj@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('JBieber@gmail.com','MariaCarey@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Iggy@gmail.com','Rihanna@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Iggy@gmail.com','AGrande@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Iggy@gmail.com','NickiMinaj@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Iggy@gmail.com','MariaCarey@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Rihanna@gmail.com','AGrande@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Rihanna@gmail.com','NickiMinaj@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Rihanna@gmail.com','MariaCarey@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('AGrande@yahoo.com','NickiMinaj@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('AGrande@yahoo.com','MariaCarey@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('NickiMinaj@gmail.com','MariaCarey@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('EmmaWatson@yahoo.com','DRadcliffe@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Timcook@gmail.com','JeffBezos@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Timcook@gmail.com','Elonmusk@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Timcook@gmail.com','LarryPage@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('JeffBezos@yahoo.com','Elonmusk@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('JeffBezos@yahoo.com','LarryPage@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Elonmusk@yahoo.com','LarryPage@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('MariaCarey@gmail.com','EmmaWatson@yahoo.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('Timcook@gmail.com','SCurry@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('ChrisBrown@gmail.com','RWestbrook@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('KHarin@gmail.com','RWestbrook@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('KHarin@gmail.com','DurantKev@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('JHarden@gmail.com','Iggy@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('JHarden@gmail.com','Rihanna@gmail.com','0')
+	INTO Friendship (intitiator, receiver, is_pending) VALUES ('DRadcliffe@gmail.com','JBieber@gmail.com','0')
+SELECT 1 FROM DUAL;
+
+INSERT ALL
+	INTO PrivateMessage (time, sent_by, received_by, body, sender_copy_delete, receiver_copy_delete) VALUES ('7.5.2016, 9:00 AM','DurantKev@gmail.com','Russel Westbrook','I am going to Warriors.','0','0')	
+	INTO PrivateMessage (time, sent_by, received_by, body, sender_copy_delete, receiver_copy_delete) VALUES ('7.8.2016, 10:00 PM','PDinklage@gmail.com','EClarke@gmail.com','We are gonna build ships and sail to Westeros.','0','0')
+	INTO PrivateMessage (time, sent_by, received_by, body, sender_copy_delete, receiver_copy_delete) VALUES ('On 4.1.2016, 9:00 AM','AGrande@yahoo.com','JBieber@gmail.com',"I'm gonna stop the rumor between us",'0','0')
+	INTO PrivateMessage (time, sent_by, received_by, body, sender_copy_delete, receiver_copy_delete) VALUES ('On 4.1.2016, 9:01 AM','JBieber@gmail.com','AGrande@yahoo.com','go ahead.','0','0')
+SELECT 1 FROM DUAL;
+
+INSERT ALL
+	INTO ChatGroup (gname, duration, owner) VALUES ('Thunder Big3','7','DurantKev@gmail.com')	
+	INTO ChatGroup (gname, duration, owner) VALUES ('Game of Thrones','7','EClarke@gmail.com')
+	INTO ChatGroup (gname, duration, owner) VALUES ('CEOs','7','Timcook@gmail.com')
+SELECT 1 FROM DUAL;
+
+INSERT ALL
+	INTO GroupMember (email, gname) VALUES ('EClarke@gmail.com','Game of Thrones')
+	INTO GroupMember (email, gname) VALUES ('PDinklage@gmail.com','Game of Thrones')
+	INTO GroupMember (email, gname) VALUES ('KHarin@gmail.com','Game of Thrones')
+	INTO GroupMember (email, gname) VALUES ('Lheadey@yahoo.com','Game of Thrones')
+	INTO GroupMember (email, gname) VALUES ('RWestbrook@gmail.com','Thunder Big3')
+	INTO GroupMember (email, gname) VALUES ('DurantKev@gmail.com','Thunder Big3')
+	INTO GroupMember (email, gname) VALUES ('JHarden@gmail.com','Thunder Big3')
+	INTO GroupMember (email, gname) VALUES ('JeffBezos@yahoo.com','CEOs')
+	INTO GroupMember (email, gname) VALUES ('Elonmusk@yahoo.com','CEOs')
+	INTO GroupMember (email, gname) VALUES ('Timcook@gmail.com','CEOs')
+	INTO GroupMember (email, gname) VALUES ('LarryPage@gmail.com','CEOs')
+SELECT 1 FROM DUAL;
+
+INSERT ALL
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.1.2016, 5:00 PM','DurantKev@gmail.com','We are moving forward.','Thunder Big3')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.1.2016, 5:01 PM','James Harden','I deserve the MVP this season!','Thunder Big3')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.1.2016, 5:02 PM','RWestbrook@gmail.com','I will lead the team to champion!','Thunder Big3')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.1.2016, 5:03 PM','DurantKev@gmail.com','LOL. Hope to see u in western finals','Thunder Big3')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.3.2016, 9:00 AM','EClarke@gmail.com','Do you guys see my dragons?','Game of Thrones')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.3.2016, 9:01 AM','PDinklage@gmail.com','They almost ate me last night...','Game of Thrones')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.3.2016, 9:02 AM','KHarin@gmail.com',"Didn't see them... fought Ramsay last night...",'Game of Thrones')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.3.2016, 9:03 AM','Lheadey@yahoo.com',"So disappointed they didn't eat Tyrion... But it doesn't matter, I am the real queen now...",'Game of Thrones')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.3.2016, 9:04 AM','PDinklage@gmail.com','All right...','Game of Thrones')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.10.2016, 10:01 AM','Timcook@gmail.com','There will be no discount on Macbook Pro!','CEOs')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.10.2016, 10:02 AM','JeffBezos@yahoo.com',"C'mon man, it's soon Black Friday...",'CEOs')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.10.2016, 10:15 AM','Elonmusk@yahoo.com','I think surface book is better!','CEOs')
+	INTO ChatGroupMessage (time, sent_by, body, gname) VALUES ('11.10.2016, 10:16 AM','JeffBezos@yahoo.com','[emoji]','CEOs') 
+SELECT 1 FROM DUAL;
+
+INSERT ALL
+	INTO CustomMessage (time, sent_by, body) VALUES ('11 Nov 2016, 6:27AM','DurantKev@gmail.com','Everything about this city keeps getting better. Hitting the streets today!')
+	INTO CustomMessage (time, sent_by, body) VALUES ('13 Nov 2016, 10:50 AM','KHarin@gmail.com','Lannisters Everywhere! Smiling Smiling Smiling')
+SELECT 1 FROM DUAL;
+
+
+INSERT ALL
+	INTO BroadcastMessage (time, sent_by, body, is_public) VALUES ('11 Nov 2016, 6:27AM','Timcook@gmail.com','Proud to work alongside these "&" many more veterans at Apple. We honor all the brave men "&" women who sacrificed for our freedom.','1','0')
+	INTO BroadcastMessage (time, sent_by, body, is_public) VALUES ('11 Nov 2016, 6:27AM','EmmaWatson@yahoo.com','So happy to show all of you the new teaser poster for Beauty and the Beast! I hope you like it. Love Emma x','1','0')
+	INTO BroadcastMessage (time, sent_by, body, is_public) VALUES ('13 Nov 2016, 10:50 AM','DurantKev@gmail.com','Lets go support our own and check out Almost Christmas!','1','0')
+SELECT 1 FROM DUAL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
